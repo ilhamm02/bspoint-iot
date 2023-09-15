cf8bcf2c7e740d15ed6054a13af53a652fc580f0
ilhamm02 <ilhamaulana24@gmail.com>
2023-09-15 08:44:37 +0700

    remove nfc reader and integrate gps sensor to firebase

--- arduino/sketch_sep11a/sketch_sep11a.ino
+++ arduino/sketch_sep11a/sketch_sep11a.ino
@@ -1,81 +1,35 @@
 #include "Network.h"
 #include "SoftwareSerial.h"
-#include "PN532_SWHSU.h"
-#include "PN532.h"
-#include "queue"
-#include "array"
+#include "TinyGPS++.h"
+#include "TinyGPSPlus.h"
+#include "SoftwareSerial.h"
 
-Network* network;
+Network *network;
 
-SoftwareSerial SWSerial(8, 7);  // RX, TX
-PN532_SWHSU pn532swhsu(SWSerial);
-PN532 nfc(pn532swhsu);
-String tagId = "None", dispTag = "None";
-byte nuidPICC[4];
+static const int RXPin = 13, TXPin = 12;
+static const uint32_t GPSBaud = 9600;
 
-const int maxCapacity = 100;
+TinyGPSPlus gps;
 
-String* passengers = new String[maxCapacity];
+SoftwareSerial ss(RXPin, TXPin);
 
 void setup() {
   Serial.begin(9600);
+  ss.begin(GPSBaud);
 
   initNetwork();
-
-  nfc.begin();
-  nfc.SAMConfig();
-
-  // network->firestoreUpdatePosition(105.85313727623817, -6.163592203837075);
-  // network->firestoreUpdateCapacity(false);
 }
 
 void loop() {
-  readNFC();
-  delay(1000);
+  while (ss.available() > 0) {
+    gps.encode(ss.read());
+    if (gps.location.isUpdated()) {
+      network->firestoreUpdatePosition(gps.location.lng(), gps.location.lat());
+    }
+  }
 }
 
-void initNetwork() {
+void initNetwork(){
   network = new Network();
   network->initWiFi();
-}
-
-void readNFC() {
-  boolean success;
-  uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };
-  uint8_t uidLength;
-  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uidLength);
-  if (success) {
-    for (uint8_t i = 0; i < uidLength; i++) {
-      nuidPICC[i] = uid[i];
-    }
-    tagId = tagToString(nuidPICC);
-    Serial.print(tagId);
-    network->firestoreUpdateCapacity(findPassanger(tagId));
-  }
-}
-
-String tagToString(byte id[4]) {
-  String tagId = "";
-  for (byte i = 0; i < 4; i++) {
-    if (i < 3) tagId += String(id[i]) + ".";
-    else tagId += String(id[i]);
-  }
-  return tagId;
-}
-
-bool findPassanger(String tagId) {
-  bool newComer = true;
-  String* currentPassenger = new String[maxCapacity];
-  int capacity = 0;
-  for (int i = 0; i < maxCapacity; i++) {
-    if (passengers[i] == tagId) newComer = false;
-    else {
-      currentPassenger[capacity] = tagId;
-      capacity++;
-    }
-  }
-
-  passengers = currentPassenger;
-
-  return newComer;
 }
